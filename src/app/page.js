1582e8e39d360cdc6fdd11456afd94d4d59d7962
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-06-02 14:09:55 +0300

    add dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,32 @@
-import AboutMeHome from '@/components/AboutMeHome/AboutMeHome';
-import Hero from '@/components/Hero/Hero';
 import dynamic from 'next/dynamic';
 // import styles from './page.module.css';
 
 const HeroDynamic = dynamic(() => import('@/components/Hero/Hero'), {
   ssr: false,
-
   loading: () => (
-    <img src="/hero/kato-blackmore-3FIFNEBTW0A-unsplash.webp"></img>
+    <img
+      src="/hero/kato-blackmore-vMUAIcZhfoo-unsplash.webp"
+      style={{
+        width: '100%',
+        height: 'auto',
+        maxWidth: '100vw',
+        maxHeight: '100vh',
+        objectFit: 'cover',
+      }}
+      alt="Loading image"
+    />
   ),
 });
+
+const DynamicAboutMeHome = dynamic(() =>
+  import('@/components/AboutMeHome/AboutMeHome')
+);
 
 export default function Home() {
   return (
     <>
       <HeroDynamic />
-      <AboutMeHome />
+      <DynamicAboutMeHome />
     </>
   );
 }
